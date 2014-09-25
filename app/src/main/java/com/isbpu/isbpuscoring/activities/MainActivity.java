a920aa0f9cef644fa47bf41e7aeefdaf63ac28d9
PatrickLove <patricklove@comcast.net>
2014-09-25 19:57:25 -0400

    Create activities package

--- app/src/main/java/com/isbpu/isbpuscoring/activities/MainActivity.java
+++ app/src/main/java/com/isbpu/isbpuscoring/activities/MainActivity.java
@@ -1,9 +1,11 @@
-package com.isbpu.isbpuscoring;
+package com.isbpu.isbpuscoring.activities;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.isbpu.isbpuscoring.R;
 
 
 public class MainActivity extends ActionBarActivity {
